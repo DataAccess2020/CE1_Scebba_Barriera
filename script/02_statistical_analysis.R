198f0664429d2b51f56f2e1d0fd7b595e9df0722
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-11-27 22:04:35 +0100

    table with chi-square

--- script/02_statistical_analysis.R
+++ script/02_statistical_analysis.R
@@ -5,3 +5,10 @@ with(Data_analysis, table(Internet_use, Age_gr))
 #Table with %
 
 round(prop.table(table(Data_analysis$Internet_use,Data_analysis$Age_gr))*100,1)
+
+#Table with chi-square 
+
+install.packages("gmodels")
+
+library(gmodels)
+CrossTable(Data_analysis$Internet_use, Data_analysis$Age_gr)
