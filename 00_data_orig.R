9c6c71d1f12b106b0b67665532b95c82219a2d15
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-11-27 21:38:11 +0100

    putting in "script"

--- 00_data_orig.R
+++
@@ -1,6 +0,0 @@
-library(rio)
-library(tidyverse)
-setwd("C:/Users/utente/OneDrive/Desktop/sara/magistrale/data access/CE1_Scebba_Barriera/data_orig")
-
-import("C:/Users/utente/OneDrive/Desktop/sara/magistrale/data access/CE1_Scebba_Barriera/data_orig/ESS9e03_1.dta")
-Data_set<- import("ESS9e03_1.dta")
